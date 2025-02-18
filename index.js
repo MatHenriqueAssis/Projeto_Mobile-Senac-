d0fc6ee94600f0f8db8dd6f076454be75f391280
Mateus Henrique de Assis <mateushenriqueaabc@gmail.com>
2025-02-17 21:33:39 -0300

    Corrigindo servidor para servir arquivos estáticos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+// Servir arquivos estáticos da pasta 'static'
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
