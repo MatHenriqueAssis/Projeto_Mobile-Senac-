2b5cbf4898372f8e569a7094f5083eefbb6f60a5
Mateus Henrique de Assis <mateushenriqueaabc@gmail.com>
2025-02-17 22:13:27 -0300

    Fim de novo

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 3000;
 // Servir arquivos estáticos corretamente
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Redirecionar "/" para "index.html"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
